27ca4969919fefb029ebdb9ad013d86507fef626
Masud99Rana <Masudrana917775@gmail.com>
2019-10-16 00:31:31 +0600

    basic config, add readme instructions, delete some default files

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Mr Ecommerce'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Dhaka',
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:Gua8RwIMxCMocmEB9EojRkdPEiUCLzi0M0GEWAtLgHg='),
 
     'cipher' => 'AES-256-CBC',
 
